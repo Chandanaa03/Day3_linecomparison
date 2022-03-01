2eb0f01c6e5d106123a66b32979ee9c1a9d73faf
chandana03 <chandanareshma01@gmail.com>
2022-03-02 01:21:12 +0530

    linelength_uc1

---
+++ linelength.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linecomparison_day3
+{
+    public class linelength
+    {
+
+        public static void lengthofline()
+        {
+
+            double x1 = 3;
+            double y1 = 4;
+            double x2 = 4;
+            double y2 = 3;
+
+           
+
+            double d =  Math.Sqrt(Math.Pow(x2 - x1, 2) +
+                              Math.Pow(y2 - y1, 2) * 1.0);
+
+
+            Console.WriteLine(" Distance :" + d );
+            
+
+
+        }
+
+
+        
+          
+            
+        
+    }
+}
