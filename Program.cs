f7c557bc37c7e4ec9d3113d4fb92e190bb7c2310
chandana03 <chandanareshma01@gmail.com>
2022-03-02 01:13:40 +0530

    Line Greeting message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace linecomparison_day3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Welcome to Line comparison");
+            //double input = Convert.ToDouble(Console.ReadLine());
+
+            //linelength.lengthofline();
+
+            // lineequity.line_equity();
+
+            equitycomparison.comparingline();
+
+            
+        }
+
+    }
+}
