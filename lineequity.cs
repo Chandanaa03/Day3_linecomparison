459c3c1868b061e6e42e843176b82d0c24b1f98e
chandana03 <chandanareshma01@gmail.com>
2022-03-02 01:23:10 +0530

    lineequity_uc2

---
+++ lineequity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linecomparison_day3
+{
+    public class lineequity
+    {
+        public static void line_equity()
+        {
+            int line1 = 5;
+            int line2 = 10;
+
+            if (line2 == line1)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("lines are unequal");
+            } 
+                
+
+
+        }
+
+    }
+}
